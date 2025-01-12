1a3108734995c0da2db640618f1d5f136835f4c8
Harsh Chaudhary <harsh.221715@ncit.edu.np>
2025-01-12 13:17:05 +0545

    updated server.js for making api

--- health_safety_app/server.js
+++ health_safety_app/server.js
@@ -14,3 +14,11 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+const ambulances = [
+    { id: 1, name: "City Ambulance", phone: "123-456-7890", location: "Downtown" },
+    { id: 2, name: "Rapid Response", phone: "987-654-3210", location: "Uptown" },
+];
+
+app.get("/api/ambulances", (req, res) => {
+    res.json(ambulances);
+});
