a6bfea48073a34c1b4d27fbaf5bf6c66e3334a84
Harsh Chaudhary <harsh.221715@ncit.edu.np>
2025-01-13 05:58:21 +0545

    Earthquake bbased alert map visualization

--- frontend/src/pages/Alerts.tsx
+++ frontend/src/pages/Alerts.tsx
@@ -141,7 +141,7 @@ const Alerts = () => {
     <div className="rounded-2xl border-2 border-black container mx-auto p-6" 
 >
 
-      <h1 className='text-4xl font-bold text-center my-8'>Earthquake Alerts Based Risk Map</h1>
+      <h1 className='text-4xl font-bold text-center my-8'>Earthquake Based Alert Map</h1>
       <div className="flex justify-center mb-8">
         <input
           type="text"
